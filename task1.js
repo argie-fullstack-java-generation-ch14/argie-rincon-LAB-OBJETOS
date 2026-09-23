ae423cdac478bc602bcc70cbde4188c69d502ffb
argierincon <argierincon@gmail.com>
2026-09-23 16:14:37 -0500

    feat: task 1 completed

--- task1.js
+++ task1.js
@@ -1,12 +1,13 @@
+/*
+1. Se les ha presentado un constructor para un objeto Mail.
+- Modifiquen el código para que el asunto sea la palabra `hola` y el mensaje sea la palabra `mundo`.
+*/
+
 function Mail(asunto, mensaje) {
   this.asunto = asunto
   this.mensaje = mensaje
 }
 
-// ¡Escribe tu código debajo de esta línea!
-
-const nuevoCorreo = new Mail()
-
-// ¡Escribe tu código encima de esta línea!
+const nuevoCorreo = new Mail("hola", "mundo")
 
 console.log(nuevoCorreo.asunto + ": " + nuevoCorreo.mensaje)
