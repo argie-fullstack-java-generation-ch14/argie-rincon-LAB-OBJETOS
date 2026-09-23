be4ddfea3e9dee73ab48b74bb6c712fe7c22cb66
argierincon <argierincon@gmail.com>
2026-09-23 16:31:17 -0500

    feat: task 4 completed

--- task4.js
+++ task4.js
@@ -1,8 +1,10 @@
-// ¡Escribe tu código debajo de esta línea!
+function Journey(inicio, fin) {
+  this.inicio = inicio;
+  this.fin = fin;
+}
 
-
-
-// ¡Escribe tu código encima de esta línea!
+const desde = process.argv[3];
+const hasta = process.argv[4];
 
 const viaje = new Journey(desde, hasta)
 
